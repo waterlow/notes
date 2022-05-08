35d609af3e8bb089132c1c9bfa425f6c5f787704
Ohta Akira <waterlow3442@gmail.com>
2022-05-08 22:26:29 +0900

    19章

--- tdd_ruby_minitest/xunit.rb
+++ tdd_ruby_minitest/xunit.rb
@@ -5,7 +5,10 @@ class TestCase # rubocop:disable Style/Documentation
     @name = name
   end
 
+  def set_up; end
+
   def run
+    set_up
     public_send(@name)
   end
 
@@ -15,20 +18,36 @@ class TestCase # rubocop:disable Style/Documentation
 end
 
 class WasRun < TestCase # rubocop:disable Style/Documentation
-  attr_reader :was_run
+  attr_reader :was_run, :was_set_up
 
   def test_method
     @was_run = 1
   end
-end
 
-class TestCaseTest < TestCase # rubocop:disable Style/Documentation
-  def test_running
-    test = WasRun.new('test_method')
-    assert(!test.was_run)
-    test.run
-    assert(test.was_run)
+  def set_up
+    @was_set_up = 1
   end
 end
 
+class TestCaseTest < TestCase # rubocop:disable Style/Documentation
+  def set_up
+    @test = WasRun.new('test_method')
+  end
+
+  def test_running
+    test.run
+    assert(test.was_run)
+  end
+
+  def test_set_up
+    test.run
+    assert(test.was_set_up)
+  end
+
+  private
+
+  attr_reader :test
+end
+
 TestCaseTest.new('test_running').run
+TestCaseTest.new('test_set_up').run
