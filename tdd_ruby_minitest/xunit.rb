9699463b68f053781a34e2bd04681c2a7f2373d0
Ohta Akira <waterlow3442@gmail.com>
2022-05-08 23:16:36 +0900

    21章

--- tdd_ruby_minitest/xunit.rb
+++ tdd_ruby_minitest/xunit.rb
@@ -8,9 +8,14 @@ class TestCase # rubocop:disable Style/Documentation
   def set_up; end
 
   def run
+    result = TestResult.new
+    result.test_started
+
     set_up
     public_send(@name)
     tear_down
+
+    result
   end
 
   def tear_down; end
@@ -20,11 +25,29 @@ class TestCase # rubocop:disable Style/Documentation
   end
 end
 
+class TestResult # rubocop:disable Style/Documentation
+  def initialize
+    @run_count = 0
+  end
+
+  def test_started
+    @run_count += 1
+  end
+
+  def summary
+    "#{@run_count} run, 0 failed"
+  end
+end
+
 class WasRun < TestCase # rubocop:disable Style/Documentation
   attr_reader :log
 
   def test_method
     @log += 'test_method '
+  end
+
+  def test_broken_method
+    raise
   end
 
   def set_up
@@ -42,6 +65,20 @@ class TestCaseTest < TestCase # rubocop:disable Style/Documentation
     test.run
     assert(test.log == 'set_up test_method tear_down ')
   end
+
+  def test_result
+    test = WasRun.new('test_method')
+    result = test.run
+    assert(result.summary == '1 run, 0 failed')
+  end
+
+  def test_failed_result
+    test = WasRun.new('test_broken_method')
+    result = test.run
+    assert(result.summary == '1 run, 1 failed')
+  end
 end
 
 TestCaseTest.new('test_template_method').run
+TestCaseTest.new('test_result').run
+# TestCaseTest.new('test_failed_result').run
