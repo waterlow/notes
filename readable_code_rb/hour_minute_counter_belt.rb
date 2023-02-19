c68c64310e7f0ea61aa4b456782e29d8137dc2ba
waterlow <waterlow3442@gmail.com>
2023-02-19 16:41:11 +0900

    ベルトコンベア版を実装

--- readable_code_rb/hour_minute_counter_belt.rb
+++ readable_code_rb/hour_minute_counter_belt.rb
@@ -12,20 +12,47 @@ class HourMinuteCounterBelt # :nodoc:
 
   def add(amount)
     @minute_events.push(Event.new(amount, now))
-    shift_events
+    shift_events(now)
     @minute_count += amount
     @hour_count += amount
+    self
   end
 
-  def minute_count; end
+  def minute_count
+    shift_events(now)
+    @minute_count
+  end
 
-  def hour_count; end
-
-  def shift_events; end
+  def hour_count
+    shift_events(now)
+    @hour_count
+  end
 
   private
 
   def now
     Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+
+  def shift_events(time)
+    shift_minute_events(time)
+    shift_hour_events(time)
+  end
+
+  def shift_minute_events(time)
+    minute_ago = time - 60
+    while @minute_events[0] && @minute_events[0].time <= minute_ago
+      @hour_events.push(@minute_events[0])
+      @minute_count -= @minute_events[0].amount
+      @minute_events.shift
+    end
+  end
+
+  def shift_hour_events(time)
+    hour_ago = time - 3600
+    while @hour_events[0] && @hour_events[0].time <= hour_ago
+      @hour_count -= @hour_events[0].amount
+      @hour_events.shift
+    end
   end
 end
