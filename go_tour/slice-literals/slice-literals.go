28382c65c56d257f420a70981d4e7d335398b88e
waterlow <waterlow3442@gmail.com>
2025-01-01 13:54:34 +0900

    TOG moretypes 9/27

--- go_tour/slice-literals/slice-literals.go
+++ go_tour/slice-literals/slice-literals.go
@@ -5,4 +5,17 @@ import "fmt"
 func main() {
 	q := []int{2,3,5,7,11}
 	fmt.Println(q)
+
+	r := []bool{true, false, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+	}
+	fmt.Println(s)
+
+	a := {2,true}
 }
